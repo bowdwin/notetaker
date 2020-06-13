350cc42a7a6a41d7835497fe704388de8386573a
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-06-13 16:37:31 -0400

    have saving to json db file working

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,6 +3,7 @@ const path = require("path");
 const router = express.Router();
 const fs = require("fs");
 const db = path.join(__dirname, "../db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/notes", (req, res) => {
   fs.readFile(db, "utf-8", (err, data) => {
@@ -12,6 +13,34 @@ router.get("/notes", (req, res) => {
     res.json(JSON.parse(data)); //return all notes to user
     console.log("/api/notes GET called");
   });
+});
+
+router.post("/notes", (req, res) => {
+  // place what the user entered into new note { title: 'keith', text: 'jon' }
+  let newNote = req.body;
+  let dbArray = []; //create an array to store things in
+
+  //read the json file
+  fs.readFile(db, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    //parse the array
+    dbArray = JSON.parse(data);
+    //assign a unique id example 9b20063c-8ecf-4f20-ad2b-a0eb6916aa4b
+    newNote.id = uuidv4();
+    //push it to db array
+    dbArray.push(newNote);
+
+    //overwrite the db.json file
+    fs.writeFile(db, JSON.stringify(dbArray), (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Successfully overwrote db.json file");
+    });
+  });
+  res.json(newNote); //newNote returned to user
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// sets variables for the routes
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
 // set up our middleware to handle POST data
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
+//always required
 app.use(express.static("public"));
+
+//tells it to use the following routes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
+//listens for server request
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
