83cc803857d633883237b93853dca1667672eea2
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-06-13 18:45:06 -0400

    configed for horoku

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 // sets variables for the routes
 const htmlRoutes = require("./routes/htmlRoutes");
