1f58ac6ea5a234dfdbfbde8484d6a6b6989a343e
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-06-13 18:27:35 -0400

    got delete working finally after a few hours, used filter function

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const db = path.join(__dirname, "../db/db.json");
 const { v4: uuidv4 } = require("uuid");
 
+//get the notes and post them to the left side
 router.get("/notes", (req, res) => {
   fs.readFile(db, "utf-8", (err, data) => {
     if (err) {
@@ -15,6 +16,7 @@ router.get("/notes", (req, res) => {
   });
 });
 
+// save notes to the notetaker app
 router.post("/notes", (req, res) => {
   // place what the user entered into new note { title: 'keith', text: 'jon' }
   let newNote = req.body;
@@ -41,6 +43,38 @@ router.post("/notes", (req, res) => {
     });
   });
   res.json(newNote); //newNote returned to user
+});
+
+router.delete("/notes/:id", (req, res) => {
+  let newNote = req.body;
+  let dbArray = []; //create an array to store things in
+
+  //read the json file
+  fs.readFile(db, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    //parson json file into dbArray , array
+    dbArray = JSON.parse(data);
+
+    //use the filter function to filter through dbArray and create a new array filterItems for ID's that did not match
+    // the request that was clicked.
+    let filteredItems = dbArray.filter((note) => {
+      return note.id != req.params.id;
+    });
+    //console log below shows the items removed in the console
+    console.log(filteredItems, "filtered items");
+
+    //re-write the json file with the filtered items
+    fs.writeFile(db, JSON.stringify(filteredItems), (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Successfully overwrote db.json file");
+    });
+  });
+  //newNote returned to user by overwritng the body
+  res.json(newNote);
 });
 
 module.exports = router;
